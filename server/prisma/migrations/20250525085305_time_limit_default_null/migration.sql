e4b77283ed1df093464e6eac4216379dd4add58b
Maksim Marychev <marycheff17@gmail.com>
2025-05-25 12:56:37 +0400

    [B] feat(gigachat): полное создание теста, запись его в БД. Prisma: Убран @default(0) у timeLimit для лучшей читаемости

---
+++ server/prisma/migrations/20250525085305_time_limit_default_null/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "test_settings" ALTER COLUMN "timeLimit" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "test_settings_snapshots" ALTER COLUMN "timeLimit" DROP DEFAULT;
--- server/src/controllers/gigaChat/gigachat.controller.ts
+++ server/src/controllers/gigaChat/gigachat.controller.ts
@@ -1,5 +1,6 @@
 import { envConfig } from "@/config/env-config"
-import { gigaChatService, gigaChatTokenService } from "@/services"
+import { ApiError } from "@/exceptions"
+import { gigaChatService, gigaChatTokenService, questionService, testService } from "@/services"
 import { NextFunction, Request, Response } from "express"
 
 class GigaChatController {
@@ -18,9 +19,19 @@ class GigaChatController {
     async generateTest(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
             const { topic, numOfQuestions } = req.body
+            const authorId = req.user?.id
+            if (!authorId) {
+                throw ApiError.Unauthorized()
+            }
+
             const token = await gigaChatTokenService.getAccessToken(envConfig.GIGACHAT_AUTH_DATA)
-            const response = await gigaChatService.generateTest(token, topic, numOfQuestions)
-            res.status(200).json(response)
+            const questions = await gigaChatService.generateTest(token, topic, numOfQuestions)
+
+            const test = await testService.createTest(authorId, { title: topic })
+
+            const updatedQuestions = await questionService.upsertQuestions(test.id, questions)
+
+            res.status(200).json({ ...test, questions: updatedQuestions })
         } catch (e) {
             next(e)
         }
